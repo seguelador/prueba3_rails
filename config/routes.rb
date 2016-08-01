a0cbf52dbbc07c305286245118a9f0bca99dc75c
seguelador <matias.seguel.estay@gmail.com>
2016-08-01 19:34:17 -0400

    name field added to User

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   resources :songs
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'users#new_user_session'
+  root 'songs#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
